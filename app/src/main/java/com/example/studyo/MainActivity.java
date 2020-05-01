67179b98d7eb9d44ec7781f7267605966f38ad8e
ALEX VUONG <vavuong@deakin.edu.au>
2020-05-01 22:21:32 +1000

    Initial commit

---
+++ app/src/main/java/com/example/studyo/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.studyo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+        bottomNav.setOnNavigationItemSelectedListener(new navItemListener());
+    }
+
+    private class navItemListener implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+        Fragment selectedScreen = null;
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            switch (menuItem.getItemId()) {
+                case R.id.action_timer:
+                    selectedScreen = new TimerScreenFragment();
+                    break;
+                default:
+                    selectedScreen = new TimerScreenFragment();
+            }
+
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, selectedScreen);
+            fragmentTransaction.commit();
+
+            return true;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/studyo/TimerScreenFragment.java
@@ -0,0 +1,28 @@
+package com.example.studyo;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TimerScreenFragment extends Fragment {
+
+    public TimerScreenFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_timer_screen, container, false);
+    }
+}
